bef046ea57472e068b45fac14f8583a94d71303e
William Lu <william.lu@shopify.com>
2018-03-28 20:01:24 -0400

    Added tests

--- test/ratings_aggregator_test.rb
+++ test/ratings_aggregator_test.rb
@@ -1,11 +1,26 @@
 require "test_helper"
+require "ratings_aggregator"
 
 class RatingsAggregatorTest < Minitest::Test
+  # Your own key is needed to run these tests
+  OMDB_KEY = 'c9005507'
+
+  def setup
+    @aggregator = RatingsAggregator::Base.new
+    @aggregator.set_key(OMDB_KEY)
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::RatingsAggregator::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_search_function_returns_proper_score
+    assert_equal 92, @aggregator.search('Wall-E')
+    assert_equal 92, @aggregator.search('Wall-E')
   end
+
+  def test_search_function_works_when_OMDB_provides_multiple_movies
+    assert_equal 76, @aggregator.search('Monster')
+  end
+    
 end
